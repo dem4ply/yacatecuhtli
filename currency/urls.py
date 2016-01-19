5e0b476de2d3997b4df7eb11d6f25984ff81cb34
dem4ply <dem4ply@gmail.com>
2016-01-19 12:38:17 -0600

    [currency] se agregaron las vistas de los bancos

--- currency/urls.py
+++ currency/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_nested import routers
-from .views import Currency_view
+from .views import Currency_view, Bank_view
 
 router = routers.SimpleRouter()
 router.register( r'currency', Currency_view, base_name='currency' )
+router.register( r'bank', Bank_view, base_name='bank' )
 
 urlpatterns = router.urls
 
--- currency/views.py
+++ currency/views.py
@@ -1,6 +1,6 @@
 # vim: set fileencoding=utf-8 :
-from .models import Currency
-from .serializers import Currency_serializer
+from .models import Currency, Bank
+from .serializers import Currency_serializer, Bank_serializer
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -14,3 +14,12 @@ class Currency_view( viewsets.ModelViewSet ):
 	permission_classes = ( IsAuthenticated, )
 	serializer_class = Currency_serializer
 	queryset = Currency.objects.all()
+
+class Bank_view( viewsets.ModelViewSet ):
+	"""
+	CRUD para la moneda
+	"""
+	authentication_classes = ( TokenAuthentication, )
+	permission_classes = ( IsAuthenticated, )
+	serializer_class = Bank_serializer
+	queryset = Bank.objects.all()
